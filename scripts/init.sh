2c68adc6a6ec52310eccd77b1b24089d014cd663
afonscosta <afonso.costa.15@gmail.com>
2018-12-13 12:05:50 +0000

    Remover a tpcc-db primeiro no init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+rm -r tpcc-db
 let "numClients = $1 * 10"
 sed -i 's/tpcc.number.warehouses=[0-9]\+/tpcc.number.warehouses='"$1"'/g' tpc-c-0.1-SNAPSHOT/etc/workload-config.properties
 sed -i 's/clients=[0-9]\+/clients='"$numClients"'/g' tpc-c-0.1-SNAPSHOT/etc/workload-config.properties
